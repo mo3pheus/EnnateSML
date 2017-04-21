abc43761fc89eca7636eecae2f5caea92d412363
Sanket Korgaonkar <sanket.korgaonkar@gmail.com>
2017-04-20 19:31:35 -0500

    CHORE: Created shell project for Neural Networks.

---
+++ SML-Sandbox/ennate.egen.neural.networks/artificial.neural.networks/src/main/java/egen/solutions/artificial/neural/networks/App.java
@@ -0,0 +1,13 @@
+package egen.solutions.artificial.neural.networks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
