eec97b5691214194b326704f0bb754c791c54473
Sanket <sanket.korgaonkar@gmail.com>
2018-03-14 07:41:44 -0500

    Feature: modified driver for deployment.

--- SML-Sandbox/ennate.egen.solutions.sml/src/main/java/ennate/egen/solutions/sml/domain/Data.java
+++ SML-Sandbox/ennate.egen.solutions.sml/src/main/java/ennate/egen/solutions/sml/domain/Data.java
@@ -1,72 +1,73 @@
 package ennate.egen.solutions.sml.domain;
 
 public class Data {
-	private Double[] fields;
-	private String classId;
-	private int numberOfFields;
+    private Double[] fields;
+    private String   classId;
+    private int      numberOfFields;
 
-	public Data(String content, String delimiter, int numberOfFields) throws NumberFormatException {
-		String[] parts = content.split(delimiter);
-		this.numberOfFields = numberOfFields;
+    public Data(String content, String delimiter, int numberOfFields) throws NumberFormatException {
+        String[] parts = content.split(delimiter);
+        this.numberOfFields = numberOfFields;
 
-		/*
-		 * Sanity check
-		 */
-		if (parts.length != numberOfFields) {
-			System.out.println(
-					"Corrupted string passed in" + content + " expected fields length = " + numberOfFields);
-			return;
-		}
+        /*
+         * Sanity check
+         */
+        if (parts.length != (numberOfFields + 1)) {
+            System.out.println(
+                    "Corrupted string passed in" + content + " expected fields length = " + numberOfFields + " actual" +
+							" length = " + parts.length);
+            return;
+        }
 
-		fields = new Double[numberOfFields];
-		int i;
-		for (i = 0; i < numberOfFields; i++) {
-			fields[i] = Double.parseDouble(parts[i]);
-		}
-		classId = parts[i];
-	}
+        fields = new Double[numberOfFields];
+        int i;
+        for (i = 0; i < numberOfFields; i++) {
+            fields[i] = Double.parseDouble(parts[i]);
+        }
+        classId = parts[i];
+    }
 
-	public Data(int numberOfFields) {
-		this.numberOfFields = numberOfFields;
-		fields = new Double[numberOfFields];
+    public Data(int numberOfFields) {
+        this.numberOfFields = numberOfFields;
+        fields = new Double[numberOfFields];
 
-		/*
-		 * Initialize the array
-		 */
-		for (int i = 0; i < numberOfFields; i++) {
-			fields[i] = new Double(0.0d);
-		}
+        /*
+         * Initialize the array
+         */
+        for (int i = 0; i < numberOfFields; i++) {
+            fields[i] = new Double(0.0d);
+        }
 
-		classId = "";
-	}
+        classId = "";
+    }
 
-	public String toString() {
-		StringBuilder dataBuilder = new StringBuilder();
-		try {
-			for (int i = 0; i < numberOfFields; i++) {
-				dataBuilder.append(fields[i]);
-				dataBuilder.append(" ");
-			}
-			dataBuilder.append(classId);
-		} catch (Exception e) {
+    public String toString() {
+        StringBuilder dataBuilder = new StringBuilder();
+        try {
+            for (int i = 0; i < numberOfFields; i++) {
+                dataBuilder.append(fields[i]);
+                dataBuilder.append(" ");
+            }
+            dataBuilder.append(classId);
+        } catch (Exception e) {
 
-		}
-		return dataBuilder.toString();
-	}
+        }
+        return dataBuilder.toString();
+    }
 
-	public Double[] getFields() {
-		return fields;
-	}
+    public Double[] getFields() {
+        return fields;
+    }
 
-	public void setFields(Double[] fields) {
-		this.fields = fields;
-	}
+    public void setFields(Double[] fields) {
+        this.fields = fields;
+    }
 
-	public String getClassId() {
-		return classId;
-	}
+    public String getClassId() {
+        return classId;
+    }
 
-	public void setClassId(String classId) {
-		this.classId = classId;
-	}
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
 }
